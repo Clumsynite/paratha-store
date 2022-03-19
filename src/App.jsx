837827ebdcd0dddcaccc8145f8dbbeed3e741e81
Rishabh Pathak <clusmsyknight@gmail.com>
2022-03-19 16:24:03 +0530

    added basic layout with Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Layout } from 'antd';
+
 import './styles/App.css';
+import Home from './Pages/Home';
+
+const { Content, Header } = Layout;
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Layout className="layout">
+      <Header>
+        <div style={{ color: '#fff', fontSize: 32, fontWeight: 'bold' }}>
+          The Paratha Store
+        </div>
+      </Header>
+      <Content
+        style={{
+          padding: '20px 50px 80px 50px',
+          overflowY: 'auto',
+          height: 'calc(100vh - 134px)',
+        }}
+      >
+        <Home />
+      </Content>
+    </Layout>
+  );
 }
 
 export default App;
