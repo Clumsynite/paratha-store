9baa3459ad188200bba40d2d596af63d3e0f3469
Rishabh Pathak <clusmsyknight@gmail.com>
2022-03-20 21:08:24 +0530

    rendered product list on checkout page

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,6 +1,12 @@
 /* eslint-disable no-nested-ternary */
-import { ArrowLeftOutlined } from '@ant-design/icons';
-import { Button, message, Row } from 'antd';
+import {
+  ArrowLeftOutlined,
+  CaretDownFilled,
+  CaretUpFilled,
+} from '@ant-design/icons';
+import {
+  Button, Col, message, Row,
+} from 'antd';
 import _ from 'lodash';
 import { func, arrayOf } from 'prop-types';
 import React, { useState, useEffect } from 'react';
@@ -8,7 +14,10 @@ import { getAllDeliveryRates } from '../api';
 import EmptyContent from '../CommonComponents/EmptyContent';
 import Loading from '../CommonComponents/Loading';
 import Text from '../CommonComponents/Text';
-import { Addon, CartProduct, Dish } from '../helper/types';
+import { getProductCost, getTotalCost } from '../helper/functions';
+import {
+  Addon, CartProduct, Dish, DishInCart,
+} from '../helper/types';
 
 export default function Checkout({
   dishes, addons, cart, onBack,
@@ -85,6 +94,67 @@ export default function Checkout({
           </div>
         </Row>
       </div>
+      <div style={{ padding: '20px 0' }}>
+        <Row
+          align="middle"
+          style={{
+            borderBottom: '1px solid #000',
+            paddingBottom: 4,
+            marginBottom: 4,
+          }}
+        >
+          <Col span={8} offset={2}>
+            <Text underline size={20}>
+              Name
+            </Text>
+          </Col>
+          <Col span={4}>
+            <Text underline size={20}>
+              Quantity
+            </Text>
+          </Col>
+          <Col span={4}>
+            <Text underline size={20}>
+              Price
+            </Text>
+          </Col>
+          <Col span={4}>
+            <Text underline size={20}>
+              Dish Total
+            </Text>
+          </Col>
+          <Col span={2}>
+            <Text underline size={20}>
+              SubTotal
+            </Text>
+          </Col>
+        </Row>
+        {productsInCart.map((product) => (
+          <DishCard key={JSON.stringify(product)} dish={product} />
+        ))}
+        <Row
+          align="middle"
+          style={{ padding: '12px 0', border: '1px solid #000' }}
+        >
+          <Col span={12} offset={2}>
+            <Text bold size={22}>
+              Cart SubTotal:
+            </Text>
+          </Col>
+          <Col span={2} offset={8}>
+            <Text bold underline size={22}>
+              {`Rs. ${getTotalCost(productsInCart)}`}
+            </Text>
+          </Col>
+        </Row>
+        <Row style={{ padding: '20px 0' }} align="middle">
+          <Col span={12}>
+            <Text bold size={22}>
+              Delivery Charges:
+            </Text>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
@@ -93,4 +163,67 @@ Checkout.propTypes = {
   addons: arrayOf(Addon).isRequired,
   cart: arrayOf(CartProduct).isRequired,
   onBack: func.isRequired,
+};
+
+function DishCard({ dish }) {
+  const [showAddons, setShowAddons] = useState(false);
+  return (
+    <div style={{ padding: '4px 0', borderBottom: '1px solid lightgrey' }}>
+      <Row align="middle" style={{ padding: '8px 0' }}>
+        <Col span={2}>
+          {dish.addons.length > 0
+            && (showAddons ? (
+              <CaretUpFilled onClick={() => setShowAddons(false)} />
+            ) : (
+              <CaretDownFilled onClick={() => setShowAddons(true)} />
+            ))}
+        </Col>
+        <Col span={8}>
+          <Text bold size={20}>
+            {dish.name}
+          </Text>
+        </Col>
+        <Col span={4}>
+          <Text bold size={20}>
+            {dish.quantity}
+          </Text>
+        </Col>
+        <Col span={4}>
+          <Text bold size={20}>
+            {`Rs. ${dish.price}`}
+          </Text>
+        </Col>
+        <Col span={4}>
+          <Text bold size={20}>
+            {`Rs. ${dish.price * dish.quantity}`}
+          </Text>
+        </Col>
+      </Row>
+      {showAddons && (
+        <div style={{ padding: '8px 0' }}>
+          {dish.addons.map((addon) => (
+            <Row align="middle" style={{ padding: '8px 0' }}>
+              <Col span={10} offset={2}>
+                <Text size={20}>{addon.name}</Text>
+              </Col>
+              <Col span={4} offset={2}>
+                <Text size={20}>{`Rs. ${addon.price}`}</Text>
+              </Col>
+            </Row>
+          ))}
+        </div>
+      )}
+      <Row align="middle">
+        <Col span={12} offset={2}>
+          <Text size={14}>Subtotal</Text>
+        </Col>
+        <Col span={2} offset={8}>
+          <Text bold underline size={20}>{`Rs. ${getProductCost(dish)}`}</Text>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+DishCard.propTypes = {
+  dish: DishInCart.isRequired,
 };
